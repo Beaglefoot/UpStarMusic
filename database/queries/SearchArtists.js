7997cc2a7030b508168c3069e456933b160b834c
Beaglefoot <ch-e-st@yandex.ru>
2018-05-14 23:38:21 +0300

    Finish SearchArtists query assignment

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,4 +9,22 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const propAlterations = {
+    name: name => name && { name: { $regex: name, $options: 'i' } },
+    age: ({ min, max }) => ({ age: { $gte: min, $lte: max } }),
+    yearsActive: ({ min, max }) => ({
+      yearsActive: { $gte: min, $lte: max }
+    })
+  };
+
+  const query = Object.entries(criteria).reduce(
+    (query, [key, value]) => Object.assign(query, propAlterations[key](value)),
+    {}
+  );
+
+  return Artist.find(query)
+    .sort(sortProperty)
+    .skip(offset)
+    .limit(limit)
+    .then(artists => ({ all: artists, count: artists.length, offset, limit }));
 };
